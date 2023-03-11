d11bfb1e29d42a209e66c08a8c98536173ac07ed
BOUTOKPO Emmanuel <tossedeemmanuelboutokpo@gmail.com>
2023-03-11 11:20:47 +0100

    Gym App

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">copyright©2023 - Tous droit reservé  </Typography>
+  </Box>
+);
+
+export default Footer;
